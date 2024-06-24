8f94c4f5693ecb0cb15c9d0302334236828cc3a9
mmmaske <mmjmaske@gmail.com>
2024-06-24 09:04:28 +0800

    Fixed uppercase-words pipe

--- src/app/uppercase-words.pipe.ts
+++ src/app/uppercase-words.pipe.ts
@@ -5,10 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UppercaseWordsPipe implements PipeTransform {
 
-  transform(value: string, ...args: unknown[]): unknown {
-    return value.split(' ').map(function(word,index){
+  transform(value: any, ...args: unknown[]): string {
+    return value.split(' ').map(function(word:string,index:number){
         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-      }).join(' ');
+    }).join(' ');
   }
 
 }
