d4927dd6cc78a631b3c2451580f12647fd1a3ba2
mmmaske <mmjmaske@gmail.com>
2024-06-24 17:21:20 +0800

    Add function to retrieve single item by doc ID

--- src/app/components/items-list/items-list.component.ts
+++ src/app/components/items-list/items-list.component.ts
@@ -1,45 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ItemService } from 'src/app/services/items.service';
 import { map } from 'rxjs/operators';
 import { Item } from 'src/app/models/item.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-items-list',
-  templateUrl: './items-list.component.html',
-  styleUrls: ['./items-list.component.css']
+    selector: 'app-items-list',
+    templateUrl: './items-list.component.html',
+    styleUrls: ['./items-list.component.css']
 })
 export class ItemsListComponent implements OnInit {
-  items?: Item[];
-  currentItem?: Item;
-  currentIndex = -1;
-  title = '';
-
-  constructor(private itemService: ItemService) { }
-
-  ngOnInit(): void {
-    this.retrieveItems();
-  }
-
-  refreshList(): void {
-    this.currentItem = undefined;
-    this.currentIndex = -1;
-    this.retrieveItems();
-  }
-
-  retrieveItems(): void {
-    this.itemService.getAll().snapshotChanges().pipe(
-      map(changes =>
+    items?: Item[];
+    currentItem?: Item;
+    currentIndex = -1;
+    title = '';
+    data = this.itemService.getAll().snapshotChanges().pipe(
+        map(changes =>
         changes.map(c =>
-          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+            ({ id: c.payload.doc.id, ...c.payload.doc.data() })
         )
-      )
-    ).subscribe(data => {
-      this.items = data;
-    });
-  }
+        )
+    );
 
-  setActiveItem(item: Item, index: number): void {
-    this.currentItem = item;
-    this.currentIndex = index;
-  }
+    constructor(
+        private itemService: ItemService,
+        private route: ActivatedRoute,
+    ) { }
+
+    ngOnInit(): void {
+        this.retrieveItems();
+    }
+
+    refreshList(): void {
+        this.currentItem = undefined;
+        this.currentIndex = -1;
+        this.retrieveItems();
+    }
+
+    retrieveItems(): void {
+        this.data.subscribe(data => {
+        this.items = data;
+        });
+    }
+
+    retrieveItem(item_id: string) {
+        const fb_item = this.itemService.getItem(item_id);
+        fb_item.then((retrieved) => {
+            retrieved.forEach((value) => {
+                return value.data();
+            });
+        });
+    }
+
+    setActiveItem(item: Item, index: number): void {
+        this.currentItem = item;
+        this.currentIndex = index;
+
+        // sample to query firestore for document
+        const item_id = item.id?item.id:'';
+        console.log('item_id');console.log(typeof(item_id));console.log(item_id);
+        const retrieved = this.retrieveItem(item_id);
+        console.log('retrieved');console.log(typeof(retrieved));console.log(retrieved);
+    }
+
 }
