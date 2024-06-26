dc2191c6dbcc4d79d1aeaa6baa2631bbd48a1a0e
mmmaske <mmjmaske@gmail.com>
2024-06-26 15:47:01 +0800

    Query item list by logged in user

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,21 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Item } from '../models/item.model';
+import { debug,loginDetails } from '../utils/common';
+import { User } from '../models/user';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ItemService {
     private dbPath = '/items';
-
+    private localUser:User = loginDetails();
     itemsRef: AngularFirestoreCollection<Item>;
+    usersRef: AngularFirestoreCollection<User>;
 
-    constructor(private db: AngularFirestore) {
+    constructor(
+        private db: AngularFirestore,
+    ) {
         this.itemsRef = db.collection(this.dbPath);
+        this.usersRef = db.collection('users');
     }
 
     getAll(): AngularFirestoreCollection<Item> {
+        if(loginDetails()) {
+            return this.getUsersItems();
+        }
         return this.itemsRef;
+    }
+
+    getUsersItems(): AngularFirestoreCollection<Item> {
+        return this.db.collection(this.dbPath,ref=>ref.where('createdById','==',`${this.localUser.uid}`));
     }
 
     async getItem(item_id:string) {
