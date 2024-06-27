f3065b7c769902f92c1d5bbca2b2fd701735f27f
mmmaske <mmjmaske@gmail.com>
2024-06-27 12:10:52 +0800

    allow usage without needing email verification

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -90,7 +90,7 @@ export class AuthService {
   // Returns true when user is looged in and email is verified
   get isLoggedIn(): boolean {
     const user = JSON.parse(localStorage.getItem('user')!);
-    return user !== null && user.emailVerified !== false ? true : false;
+    return user !== null /*&& user.emailVerified !== false*/ ? true : false;
   }
 
   /* Setting up user data when sign in with username/password,
